bbe527788d79d3daaedec4e1f67741be7987e58a
SevakAvet <sevak.avet@gmail.com>
2014-08-30 19:11:58 +0400

    Constants class

---
+++ src/main/java/org/ssutt/android/activity/Constants.java
@@ -0,0 +1,24 @@
+package org.ssutt.android.activity;
+
+public class Constants {
+    public static final String DEPARTMENT = "department";
+    public static final String GROUP = "group";
+    public static final String DEPARTMENT_FULL_NAME = "department_full_name";
+
+    public static final String MY_DEPARTMENT = "myDepartment";
+    public static final String MY_GROUP = "myGroup";
+    public static final String MY_DEPARTMENT_FULL_NAME = "myDepartmentFullName";
+
+    public static final String FIRST_TIME = "firstTime";
+    public static final String FOR_SEARCH = "forSearch";
+
+    public static final String PREF = "pref";
+    public static final String STAR = "star";
+
+    public static final String CACHED_DEPARTMENTS = "cacheDepartments";
+    public static final String CACHED_GROUPS = "cacheGroups";
+    public static final String CACHED_SCHEDULE = "cacheSchedule";
+    public static final String CACHED_MSG = "cachedMsg";
+
+
+}
